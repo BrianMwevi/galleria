9f8ebbca09157a2eef9525fa376734e5d394d412
brianmwevi <mwevibrian@gmail.com>
2022-05-30 23:07:44 +0300

    ft:Image model -- add fields and crud methods

---
+++ galleria/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.db.models.functions import Lower
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Image(models.Model):
+    photo = models.ImageField(upload_to='galleries/', blank=False)
+    name = models.CharField(max_length=55, blank=False, null=False)
+    description = models.TextField(max_length=200, blank=True, null=False)
+    category = models.ForeignKey(
+        'Category', related_name='image', on_delete=models.CASCADE)
+    location = models.ForeignKey(
+        'Location', related_name='image', on_delete=models.CASCADE)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save_image(self):
+        self.save()
+        return self
+
+    @classmethod
+    def update_image(cls, obj):
+        image = cls.get_image_by_id(obj.id)
+        image.photo = obj.photo
+        image.name = obj.name
+        image.description = obj.description
+        image.location = obj.location
+        image.category = obj.category
+        image.save_image()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.get(id=id)
+        return image
+
+    @classmethod
+    def search_image(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
+
+    @classmethod
+    def filter_by_location(cls, location):
+        images = cls.objects.filter(location__name=location).order_by('-created_at')
+        return images
+
+    def __str__(self):
+        return self.name
