a1aac24b51a857356baa4d1624893b5f1cd1d293
brianmwevi <mwevibrian@gmail.com>
2022-05-30 23:04:54 +0300

    ft:admin dashboard -- register Image, Category and Location models

---
+++ galleria/admin.py
@@ -0,0 +1,8 @@
+from re import L
+from django.contrib import admin
+from galleria.models import Image, Category, Location
+
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
+# Register your models here.
