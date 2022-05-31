b919d4fce1a4ef894941873296e0c444271a00c0
brianmwevi <mwevibrian@gmail.com>
2022-05-31 11:35:04 +0300

    deployment

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,6 @@ git push heroku main
 
 # Django Settings
 heroku config:set SECRET_KEY='django-insecure-h-4_vq % @6x462t8ly = k = +8os_54n7_lziad!i4 *$_rey9b@1mb'
-# Cloudinary connection -- for images upload
-
 # heroku config:set CLOUD_NAME=da7srpwm6
 # heroku config:set API_KEY=123334979696849
 # heroku config:set API_SECRET=Jvd78nFKUziejHAW-b5Qn0gLuHk
