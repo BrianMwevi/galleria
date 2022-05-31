9b4584f19a23dcd03c8ff34eb52e05feb5948611
brianmwevi <mwevibrian@gmail.com>
2022-05-31 11:00:04 +0300

    deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -2,6 +2,7 @@ import dj_database_url
 from pathlib import Path
 import environ
 import os
+from decouple import config
 
 
 # Initialise environment variables
@@ -16,12 +17,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
