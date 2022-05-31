c68a7936a9a30ec950e7fd9360c665abd1ae37df
brianmwevi <mwevibrian@gmail.com>
2022-05-31 16:05:03 +0300

    deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-
+ALLOWED_HOSTS = ['jango-galleria.herokuapp.com', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
