dd365acd4d08e9e9687e57cfa03069e213ad305e
brianmwevi <mwevibrian@gmail.com>
2022-05-31 15:15:52 +0300

    deployment

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,13 @@ git push heroku main
 
 # Django Settings
 # heroku config:set SECRET_KEY='django-insecure-h-4_vq % @6x462t8ly = k = +8os_54n7_lziad!i4 *$_rey9b@1mb'
-heroku config:set CLOUD_NAME=da7srpwm6
-heroku addons:create heroku-postgresql
+# heroku config:set CLOUD_NAME=da7srpwm6
+# heroku addons:create heroku-postgresql
 
 # heroku config:set API_KEY=123334979696849
 # heroku config:set API_SECRET=Jvd78nFKUziejHAW-b5Qn0gLuHk
 # heroku config:set CLOUDINARY_URL=cloudinary://123334979696849:Jvd78nFKUziejHAW-b5Qn0gLuHk@da7srpwm6
 # # pythone manage
+
+heroku run python manage.py makemigrations
+heroku run python manage.py migrate
