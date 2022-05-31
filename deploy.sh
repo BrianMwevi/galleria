e913e1db1702ad978f1b2f859d799dcd8d39bc03
brianmwevi <mwevibrian@gmail.com>
2022-05-31 11:01:03 +0300

    deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+pip freeze > requirements.txt
 git add .
 git commit -m "deployment"
 git push heroku main
@@ -7,8 +8,8 @@ git push heroku main
 SECRET_KEY=django-insecure-h-4_vq % @6x462t8ly = k = +8os_54n7_lziad!i4 *$_rey9b@1mb
 # Cloudinary connection -- for images upload
 
-heroku config:set CLOUD_NAME=da7srpwm6
-heroku config:set API_KEY=123334979696849
-heroku config:set API_SECRET=Jvd78nFKUziejHAW-b5Qn0gLuHk
-heroku config:set CLOUDINARY_URL=cloudinary://123334979696849:Jvd78nFKUziejHAW-b5Qn0gLuHk@da7srpwm6
+# heroku config:set CLOUD_NAME=da7srpwm6
+# heroku config:set API_KEY=123334979696849
+# heroku config:set API_SECRET=Jvd78nFKUziejHAW-b5Qn0gLuHk
+# heroku config:set CLOUDINARY_URL=cloudinary://123334979696849:Jvd78nFKUziejHAW-b5Qn0gLuHk@da7srpwm6
 # pythone manage
